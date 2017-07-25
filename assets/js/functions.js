b612a0d7f1fb50c67a1a31d0b5f3227247856a89
zhujing-wang <wangzhujingwzj@gmail.com>
2017-07-25 17:36:42 +0800

    sticky things work

---
+++ assets/js/functions.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.top_container').stickMe();
+})
+$(document).ready(function() {
+
+	$(".pinBox").pinBox({
+		Top : '50px',
+		Container : '#pinBoxContainer',
+	});
+
+});
