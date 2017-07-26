3c1232b596336b7cd1fcf495ed3d5516248b3456
zhujing-wang <wangzhujingwzj@gmail.com>
2017-07-26 15:38:26 +0800

    about section mostly done

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
   $('.top_container').stickMe();
 })
-$(document).ready(function() {
-
-	$(".pinBox").pinBox({
-		Top : '50px',
-		Container : '#pinBoxContainer',
-	});
-
-});
